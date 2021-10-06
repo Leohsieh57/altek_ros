2c0da0d6a2cc0b5dc338c5d23dcaf40654b52ce6
bardiche4768@gmail.com <bardiche4768@gmail.com>
2021-10-06 10:09:19 +0800

    modifying ros node

--- src/altek_ros_main.py
+++ src/altek_ros_main.py
@@ -2,33 +2,21 @@
 
 import os
 import rospy
-from altek_ros.srv import *
+from altek_ros.srv import start_publish
 from datetime import datetime
-from sensor_msgs.msg import Image, CameraInfo, NavSatFix, CompressedImage
-
+from sensor_msgs.msg import Image
 import cv2
 import numpy as np
-import time
 import threading
 from queue import Queue
-# 20210922 #
-# support task run
-# 20210917 #
-# support image preview
-#
-gMainVer = "[py - 20210922]"
 
 gDistInMin = 200
 gDistInMax = 5000
 dev = rospy.get_param('altek/video_port') #202: default, 1: two more camera and manual set to 1.
 cnt = 10
-g_SaveDist = 0
 g_wait_ms = 1
 g_wait_1s = 1000
-DepthLUT_table = [0]*1024
 ColorLUT_table = [0]*1024*3
-g_mode=2 # 1: depth preview only, 2: depth + image preview.
-g_IsStop=0
 
 g_w_dist = 640.0
 g_h_dist = 360.0
@@ -74,57 +62,13 @@ def CV2msg(cv_image, encoding="passthrough"):
     image_message = cv2_to_imgmsg(cv_image, encoding)
     return image_message
 
-def cbDepth_altek(msg):
-    print("receive depth_altek!")
-    print("altek s: ",msg.header.stamp.secs)
-    print("altek ns: ",msg.header.stamp.nsecs)
-    
-    # cv2.imshow('Distance', cvimgDepth)
-    # cv2.waitKey(1)
 
-def cbColor_altek(msg):
-    print("receive color_altek!")
-    print("altek s: ",msg.header.stamp.secs)
-    print("altek ns: ",msg.header.stamp.nsecs)
-
-def ShowVersions():
-    print(gMainVer)
-    print('cv2 ver=%s' % cv2.__version__)
-    print('numpy ver=%s' % np.__version__)
-
-def opencv_cfg(cap, g_mode):
+def opencv_cfg(cap):
     cap.set(cv2.CAP_PROP_FORMAT, -1.0)
     cap.set(cv2.CAP_PROP_CONVERT_RGB, 0.0)
     cap.set(cv2.CAP_PROP_MODE, 0.0)
-    if g_mode == 1:
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, g_w_dist)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, g_h_dist)
-    else:
-        cap.set(cv2.CAP_PROP_FRAME_WIDTH, g_w_dist)
-        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, g_h_nv21_dist)
-
-def get_opencv_cfg():
-    vcformat = cap.get(cv2.CAP_PROP_FORMAT)
-    fourcc = cap.get(cv2.CAP_PROP_FOURCC)
-    ConvertRGB = cap.get(cv2.CAP_PROP_CONVERT_RGB)
-    Mode = cap.get(cv2.CAP_PROP_MODE)
-    W = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-    H = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-    print("Get cv2 property:")
-    print("vcformat = %f" % vcformat)
-    print("fourcc = %f" % fourcc)
-    print("ConvertRGB = %f" % ConvertRGB)
-    print("Mode = %f" % Mode)
-    print("W = %f, H = %f\n" % (W, H))
-
-def LoadDepthLUT_Table():
-    i=0
-    fn = os.path.join(lut_path, 'depth_lut.txt')
-    rf = open(fn, 'r')
-    for line in rf.readlines():
-        DepthLUT_table[i] = line
-        i = i+1
-    rf.close()
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, g_w_dist)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, g_h_nv21_dist)
 
 def LoadColorLUT_Table():
     i=0
@@ -151,35 +95,18 @@ def PixelCvt_Dist2RGBnp(Inframe, width, height, pRGB, InMin, InMax, mapping):
     ftmp[Inframe > InMax, 1] = 0 # G
     ftmp[Inframe > InMax, 2] = 0 # R
 
-def PixelCvt_Dist2Depth(Inframe, width, height, InMin, mapping):
-    depthlut = np.zeros((width,height), np.uint16)
-    depthlut = 1023 - mapping[Inframe - InMin]
-    print(depthlut.shape, depthlut.dtype)
-    return depthlut.astype(np.uint16)
 
 def CheckCamera(cap, dev, cnt):
     # Check camera open status
     while not cap.isOpened() and not rospy.is_shutdown():
         cv2.waitKey(g_wait_1s)
         cap = cv2.VideoCapture(dev, cv2.CAP_ANY)
-        print("Wait for opening camera...")
+        rospy.logwarn("Wait for opening camera...")
         if cnt <= 0:
             break
     return cnt
 
 def cv2_to_imgmsg(cvim, encoding = "passthrough"):
-        """
-        Convert an OpenCV :cpp:type:`cv::Mat` type to a ROS sensor_msgs::Image message.
-        :param cvim:      An OpenCV :cpp:type:`cv::Mat`
-        :param encoding:  The encoding of the image data, one of the following strings:
-           * ``"passthrough"``
-           * one of the standard strings in sensor_msgs/image_encodings.h
-        :rtype:           A sensor_msgs.msg.Image message
-        :raises CvBridgeError: when the ``cvim`` has a type that is incompatible with ``encoding``
-        If encoding is ``"passthrough"``, then the message has the same encoding as the image's OpenCV type.
-        Otherwise desired_encoding must be one of the standard image encodings
-        This function returns a sensor_msgs::Image message on success, or raises :exc:`cv_bridge.CvBridgeError` on failure.
-        """
         if not isinstance(cvim, (np.ndarray, np.generic)):
             raise TypeError('Your input type is not a numpy array')
 
@@ -208,110 +135,66 @@ def th_showimg():
     global cap
     global g_h_dist
     global g_h_nv21
-    global g_mode
-    global frame_cv
     global gDistInMin, gDistInMax
     global mapping
-    global g_SaveDist
-    global g_IsStop
     global fnIndex
     
     w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    print("th w=%d, h=%d, g_mode=%d" % (w, h, g_mode))
-    
-    if g_mode == 1:
-        while(g_IsStop == 0 and not rospy.is_shutdown()):
-            #tic1=time.time()
-            #print("cycle-t: ",tic1)
-            #tic2=tic1
-
-            # print("ONly preview depth...")
-
-            # get 1 image from camera
-            ret, frame_cv = cap.read()
-            if ret == 0 :
-                print("No video frame")
-                ch = cv2.waitKey(g_wait_1s)
-                continue
-            #tic2=time.time()
         
-            imgSize = (w,h)
-            frame_np = np.frombuffer(frame_cv, dtype=np.uint16).reshape(imgSize)
-            fBGR = np.zeros((h,w,3), np.uint8)
-            PixelCvt_Dist2RGBnp(frame_np, w, h, fBGR, gDistInMin, gDistInMax, mapping)
-            q_d.put(fBGR)
+    while not rospy.is_shutdown():
+        # get 1 image from camera
+        ret, frame_cv = cap.read()
+        if not ret:
+            rospy.logerr("No video frame received! ")
+        
+        imgSize = (h*2,w,1)
+        frame_np = np.frombuffer(frame_cv, dtype=np.uint8).reshape(imgSize)
+        h_rgb = int(g_h_nv21)
+        ftmp = frame_np[:h_rgb,:w]
+        fBGRnv21 = cv2.cvtColor(ftmp,cv2.COLOR_YUV2BGR_NV21)
             
-            if(g_SaveDist == 1):
-                fcv = open('./Image/frame_dist.raw','wb')
-                fcv.write(frame_cv)
-                fcv.close()
-                print("Write frame_cv.raw")
-                g_SaveDist = 0
-        
-    else:
-        while(g_IsStop == 0 and not rospy.is_shutdown()):
-            #tic1=time.time()
-            #print(tic1)
-            #tic2=tic1
-        
-            # get 1 image from camera
-            ret, frame_cv = cap.read()
-            if ret == 0 :
-                print("No video frame")
-                ch = cv2.waitKey(g_wait_1s)
-                continue
-            #tic2=time.time()
-        
-            imgSize = (h*2,w,1)
-            frame_np = np.frombuffer(frame_cv, dtype=np.uint8).reshape(imgSize)
-            h_rgb=int(g_h_nv21)
-            ftmp = frame_np[:h_rgb,:w]
-            fBGRnv21 = cv2.cvtColor(ftmp,cv2.COLOR_YUV2BGR_NV21)
-            
-            fdist = frame_np[h_rgb:h*2,:w]
-            h_d=int(g_h_dist)
-            frame_npd = np.frombuffer(fdist, dtype=np.uint16).reshape(h_d, w)
+        fdist = frame_np[h_rgb:h*2,:w]
+        h_d=int(g_h_dist)
+        frame_npd = np.frombuffer(fdist, dtype=np.uint16).reshape(h_d, w)
             #fBGRdt = np.zeros((h_d,w,3), np.uint8)
             #PixelCvt_Dist2RGBnp(frame_npd, w, h_d, fBGRdt, gDistInMin, gDistInMax, mapping)
             # merge depth + image.
             # fBGR = np.concatenate((fBGRnv21, fBGRdt), axis=1)
-            q_d.put(frame_npd)
-            q_rgb.put(fBGRnv21)
+        q_d.put(frame_npd)
+        q_rgb.put(fBGRnv21)
 
-            if save_data:
-                fn = str(fnIndex).zfill(6)
+        if save_data:
+            fn = str(fnIndex).zfill(6)
 
-                fBGRdt = np.zeros((h_d,w,3), np.uint8)
-                PixelCvt_Dist2RGBnp(frame_npd, w, h_d, fBGRdt, gDistInMin, gDistInMax, mapping)
+            fBGRdt = np.zeros((h_d,w,3), np.uint8)
+            PixelCvt_Dist2RGBnp(frame_npd, w, h_d, fBGRdt, gDistInMin, gDistInMax, mapping)
 
-                fcv = open(os.path.join(path_dict['color_raw'], fn+'.raw'),'wb')
-                fcv.write(ftmp)
-                fcv.close()
+            fcv = open(os.path.join(path_dict['color_raw'], fn+'.raw'),'wb')
+            fcv.write(ftmp)
+            fcv.close()
 
-                fcvd = open(os.path.join(path_dict['depth_raw'], fn+'.raw'),'wb')
-                fcvd.write(fdist)
-                fcvd.close()
+            fcvd = open(os.path.join(path_dict['depth_raw'], fn+'.raw'),'wb')
+            fcvd.write(fdist)
+            fcvd.close()
 
-                np.save(os.path.join(path_dict['depth_npy'], fn+'.npy'), fBGRdt)
-                np.save(os.path.join(path_dict['color_npy'], fn+'.npy'), fBGRnv21)
+            np.save(os.path.join(path_dict['depth_npy'], fn+'.npy'), fBGRdt)
+            np.save(os.path.join(path_dict['color_npy'], fn+'.npy'), fBGRnv21)
 
-                cv2.imwrite(os.path.join(path_dict['depth_img'], fn+'.png'), fBGRdt)
-                cv2.imwrite(os.path.join(path_dict['color_img'], fn+'.png'), fBGRnv21)
+            cv2.imwrite(os.path.join(path_dict['depth_img'], fn+'.png'), fBGRdt)
+            cv2.imwrite(os.path.join(path_dict['color_img'], fn+'.png'), fBGRnv21)
 
-                print("Write frame_cv.raw")
+            print("Write frame_cv.raw")
 
-                fnIndex+=1
+            fnIndex+=1
 
 def wait_for_cmd(msg):
     global wait_for_publish_cmd
     wait_for_publish_cmd = False
     return wait_for_publish_cmd
 
-# show versions
-if __name__ == "__main__":
-    ShowVersions()
 
+if __name__ == "__main__":
     rospy.init_node("altek_ros", anonymous=True)
     pubDepth = rospy.Publisher("/Altek/depth/image_rect_raw", Image, queue_size=100)
     pubColor = rospy.Publisher("/Altek/color/image_raw", Image, queue_size=100)
@@ -334,11 +217,9 @@ if __name__ == "__main__":
     rospy.loginfo("Start publish ROS topic! ")
     if cap.isOpened() and not rospy.is_shutdown():
         cnt = 0
-        opencv_cfg(cap, g_mode)
+        opencv_cfg(cap)
 
-        #LoadDepthLUT_Table()
         LoadColorLUT_Table()
-        get_opencv_cfg()
         
         table_size = gDistInMax - gDistInMin + 1
         mappinglist = [0]*65536 #table_size
@@ -352,53 +233,27 @@ if __name__ == "__main__":
         t = threading.Thread(target = th_showimg)
         t.start()
         
-        while(not rospy.is_shutdown()):
+        while not rospy.is_shutdown():
 
-            if g_mode == 1:
+            fDepth = q_d.get()
+            fBGR = q_rgb.get()
 
-                fDepth = q_d.get()
-                # cv2.namedWindow('Distance', 1)
-                # cv2.imshow('Distance', fBGR)
+            #cvt to rosmsg
+            now = rospy.get_rostime()
+            msgDepth = CV2msg(fDepth)
+            msgDepth.header.stamp.secs = now.secs
+            msgDepth.header.stamp.nsecs = now.nsecs
+            msgDepth.header.frame_id = 'camera_link'
+            pubDepth.publish(msgDepth)
 
-                '''cvt to rosmsg'''
-                now = rospy.get_rostime()
-                msgDepth = CV2msg(fDepth)
-                msgDepth.header.stamp.secs = now.secs
-                msgDepth.header.stamp.nsecs = now.nsecs
-                msgDepth.header.frame_id = 'camera_link'
-                pubDepth.publish(msgDepth)
-            
-            else:
-                fDepth = q_d.get()
-                fBGR = q_rgb.get()
-
-                '''cvt to rosmsg'''
-                now = rospy.get_rostime()
-                msgDepth = CV2msg(fDepth)
-                msgDepth.header.stamp.secs = now.secs
-                msgDepth.header.stamp.nsecs = now.nsecs
-                msgDepth.header.frame_id = 'camera_link'
-                pubDepth.publish(msgDepth)
-
-                msgColor = CV2msg(fBGR)
-                msgColor.header.stamp.secs = now.secs
-                msgColor.header.stamp.nsecs = now.nsecs
-                msgColor.header.frame_id = 'camera_link'
-                pubColor.publish(msgColor)
-
-                
-            # exit while loop when press q
-            ch = cv2.waitKey(g_wait_ms)
-            if ch& 0xFF == 27: #ESC key
-                g_IsStop = 1
-                break
-            elif ch& 0xFF == ord('s'):
-                g_SaveDist = 1
-            
-            cnt=cnt+1
+            msgColor = CV2msg(fBGR)
+            msgColor.header.stamp.secs = now.secs
+            msgColor.header.stamp.nsecs = now.nsecs
+            msgColor.header.frame_id = 'camera_link'
+            pubColor.publish(msgColor)
 
     else:
-        print("Open camera fail!")
+        rospy.logerr("Open camera failure.. ")
         
     # wait thread stop.
     t.join()
@@ -407,6 +262,3 @@ if __name__ == "__main__":
     # Release queue buffer
     q_rgb.put(0)
     q_d.put(0)
-
-    # Close all OpenCV windows
-    cv2.destroyAllWindows()
